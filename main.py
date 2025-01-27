0edf01b12ea7bfd961a9345119abbcf0a181c992
Kev <kb21lh44vb77@icloud.com>
2025-01-26 21:07:17 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+
+# First Python | kevstahjr.com
+
+# msg = "Hello world!"
+# name = "Kev"
+# year = 2025
+
+age = input("How old are you? ")
+
+try:
+    per_age = int(age)
+    print("You are " + str(per_age) + " years old")
+except:
+    print("Please input a valid age!")
+
+# print("Hello, " + name + " the current year is... " + str(year))
+
+# print("Hello world")
+# print(msg)
+# print("Hello, " + name)
